58e74ad7a77edd360626bd47f5b6037480835fe0
Ilja Malenkov <iljamalenkov@gmail.com>
2012-09-14 17:04:28 +0400

    перенес запросы температуры и реле в подпрограммы

--- _8relei2c.ino
+++ _8relei2c.ino
@@ -1,7 +1,7 @@
 // Variables will change:
 int ledState = LOW;             // ledState used to set the LED
 long previousMillis = 0;        // will store last time LED was updated
-
+int tt=0;
 // the follow variables is a long because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
 long interval = 1000;           // interval at which to blink (milliseconds)
@@ -45,55 +45,15 @@ pinMode(9, OUTPUT);
 
 void loop()
 {
-  // here is where you'd put code that needs to be running all the time.
-
-  // check to see if it's time to blink the LED; that is, if the 
-  // difference between the current time and last time you blinked 
-  // the LED is bigger than the interval at which you want to 
-  // blink the LED.
-     if (Serial.available() > 0) {
-char q={Serial.read()- '0'};
-    if (q==49) {pin=2;}
-    if (q==50) {pin=3;}
-    if (q==51) {pin=4;}
-    if (q==52) {pin=5;}
-    if (q==53) {pin=6;}
-    if (q==54) {pin=7;}
-    if (q==55) {pin=9;}
-    if (q==56) {pin=10;}
-    if (q==1) {digitalWrite(pin, HIGH); Serial.print(pin); Serial.println(" on");}
-    if (q==0) {digitalWrite(pin, LOW);  Serial.print(pin); Serial.println(" off");}
-    }
-   delay (100);
+ serialRead();
+   delay (10);
    
     // метод update заставляет сенсор выдать текущие измерения
-    sensor.update();
-
-    switch (sensor.getLastError())
-    {
-        case DHT_ERROR_OK:
-            char msg[128];
-            // данные последнего измерения можно считать соответствующими
-            // методами
-            sprintf(msg, "Temperature = %dC, Humidity = %d%%", 
-                    sensor.getTemperatureInt(), sensor.getHumidityInt());
-            Serial.println(msg);
-            break;
-        case DHT_ERROR_START_FAILED_1:
-            Serial.println("Error: start failed (stage 1)");
-            break;
-        case DHT_ERROR_START_FAILED_2:
-            Serial.println("Error: start failed (stage 2)");
-            break;
-        case DHT_ERROR_READ_TIMEOUT:
-            Serial.println("Error: read timeout");
-            break;
-        case DHT_ERROR_CHECKSUM_FAILURE:
-            Serial.println("Error: checksum error");
-            break;
-    }
-
-    delay(3000);
+    if (tt==500) tempRead();
+    if (tt>500) tt=0;
+   
+tt++;
+    
 }
 
 // function that executes whenever data is received from master
@@ -108,3 +68,47 @@ void receiveEvent(int howMany)
   x = Wire.read();    // receive byte as an integer
   Serial.println(x);         // print the integer
 }
+
+void tempRead ()
+  {  sensor.update();
+
+    switch (sensor.getLastError())
+    {
+        case DHT_ERROR_OK:
+            char msg[128];
+            // данные последнего измерения можно считать соответствующими
+            // методами
+            sprintf(msg, "Temperature = %dC, Humidity = %d%%", 
+                    sensor.getTemperatureInt(), sensor.getHumidityInt());
+            Serial.println(msg);
+            break;
+        case DHT_ERROR_START_FAILED_1:
+        
+            Serial.println("Error: start failed (stage 1)");
+             sensor.attach(A1);
+                delay (100);
+            break;
+        case DHT_ERROR_START_FAILED_2:
+            Serial.println("Error: start failed (stage 2)");
+            break;
+        case DHT_ERROR_READ_TIMEOUT:
+            Serial.println("Error: read timeout");
+            break;
+        case DHT_ERROR_CHECKSUM_FAILURE:
+            Serial.println("Error: checksum error");
+            break;
+    }}
+    void serialRead(){
+       if (Serial.available() > 0) {
+char q={Serial.read()- '0'};
+    if (q==49) {pin=2;}
+    if (q==50) {pin=3;}
+    if (q==51) {pin=4;}
+    if (q==52) {pin=5;}
+    if (q==53) {pin=6;}
+    if (q==54) {pin=7;}
+    if (q==55) {pin=9;}
+    if (q==56) {pin=10;}
+    if (q==1) {digitalWrite(pin, HIGH); Serial.print(pin); Serial.println(" on");}
+    if (q==0) {digitalWrite(pin, LOW);  Serial.print(pin); Serial.println(" off");}
+    }}
